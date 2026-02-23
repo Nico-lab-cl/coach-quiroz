e3256e103aa545e3b3700fdc89efc9b08b8f9a46
Nico-lab-cl <nicolas.cab.v@gmail.com>
2026-02-23 00:04:10 -0300

    chore: instalar Google Tag Manager en layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -20,6 +20,17 @@ export default function RootLayout({ children }) {
     return (
         <html lang="es" className="scroll-smooth">
             <head>
+                {/* Google Tag Manager */}
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WV6XQMSK');`,
+                    }}
+                />
+                {/* End Google Tag Manager */}
                 <link
                     rel="stylesheet"
                     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
@@ -28,6 +39,16 @@ export default function RootLayout({ children }) {
             <body
                 className={`${outfit.className} bg-[#0a0a0a] text-white antialiased overflow-x-hidden`}
             >
+                {/* Google Tag Manager (noscript) */}
+                <noscript>
+                    <iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-WV6XQMSK"
+                        height="0"
+                        width="0"
+                        style={{ display: 'none', visibility: 'hidden' }}
+                    ></iframe>
+                </noscript>
+                {/* End Google Tag Manager (noscript) */}
                 {children}
             </body>
         </html>
