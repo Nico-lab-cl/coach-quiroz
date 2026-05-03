8fcbf5f4945a1207aa9884026307c3445e5d9424
Nico-lab-cl <nicolas.cab.v@gmail.com>
2026-05-02 23:37:38 -0400

    seo: shorten metatitle for junior tenis page

--- app/programas/junior-tenis/layout.jsx
+++ app/programas/junior-tenis/layout.jsx
@@ -1,5 +1,5 @@
 export const metadata = {
-    title: 'Junior Tenis en Viña del Mar - Entrenamiento Profesional con Coach Quiroz',
+    title: 'Junior Tenis en Viña del Mar - Entrenamiento Profesional',
     description: 'Descubre el programa de Junior Tenis de By Coach Quiroz en Viña del Mar. Ofrecemos formación deportiva integral para niños y jóvenes, adaptada a sus necesidades. ¡Inscríbete hoy y construye una base sólida en tenis!',
 };
 
